5a41a2cedc73d539cd8a47a4728d152e8dfd598a
Isaiah Norton <isaiah@tiledb.com>
2022-03-28 03:19:40 +0000

    WIP add new hypothesis test for 2d dense comparing refactored and legacy
    WIP update

--- tiledb/tests/strategies.py
+++ tiledb/tests/strategies.py
@@ -14,12 +14,21 @@ def bounded_ntuple(draw, *, length=1, min_value=0, max_value=10):
 
     return draw(st.tuples(*[st.integers(min_value, max_value) for _ in range(length)]))
 
+@composite
+def bounded_anytuple(draw, *, min_value=0, max_value=10):
+    """hypothesis composite strategy that returns a `length` tuple of integers
+    within the range (min_value, max_value)
+    """
+
+    n = draw(st.integers(min_value=1, max_value=100))
+    return draw(st.tuples(*[st.integers(min_value, max_value) for _ in range(n)]))
 
 @composite
 def ranged_slices(draw, min_value=0, max_value=10):
-    bdd = st.one_of(st.none(), st.integers(min_value=min_value, max_value=max_value))
-    start = draw(bdd)
-    stop = draw(bdd)
-    step = draw(bdd)
+    bdd = st.integers(min_value=min_value, max_value=max_value)
+    start = draw(bdd.filter(lambda x: x != None))
+    stop = draw(bdd.filter(lambda x: x != None))
+    start,stop = sorted((start,stop))
+    #step = draw(bdd)
 
-    return slice(start, stop, step)
+    return slice(start, stop, None)
